7c61c358ff43cf8a6643ce3f48279e73150d3263
Cristopher Ferrari <ferraricristopher93@gmail.com>
2024-03-18 14:07:48 +0100

    Jumbo, add img and build structure(wip)

--- src/components/AppJumbo.vue
+++ src/components/AppJumbo.vue
@@ -4,6 +4,27 @@ export default {
 };
 </script>
 <template>
-  <h1>Jumbotron</h1>
+  <section class="jumbotron">
+    <div class="container">
+      <div class="row">
+        <div class="container col-sm-12 col-xl-6">
+          <img
+            src="../../public/images/jumbooCibo.png"
+            alt="Burgher e patatine"
+          />
+        </div>
+        <div class="container col-sm-12 col-xl-6">
+          <h1>titolo</h1>
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
-<style></style>
+<style>
+.jumbotron {
+  background-color: #ffce6b;
+  img {
+    max-width: 100%;
+  }
+}
+</style>
