ea9b5630e766fe0d5bf858043081423ffb4c3622
jacopobiagi <jacopo.biagi02@gmail.com>
2024-03-18 12:28:47 +0100

    add: logo

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -7,7 +7,7 @@
     <p class="col-md-4 mb-0 text-muted">© 2022 Company, Inc</p>
 
     <a href="/" class="col-md-4 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto link-dark text-decoration-none">
-      <svg class="bi me-2" width="40" height="32"><use xlink:href="#bootstrap"></use></svg>
+      <img src="https://dhhvideos.s3.eu-central-1.amazonaws.com/Delivery-Hero-Comet.png" alt="">
     </a>
 
     <ul class="nav col-md-4 justify-content-end">
@@ -20,6 +20,14 @@
   </footer>
 </template>
 
-<style>
+<style lang="scss" scoped>
+footer{
+    padding: 15px;
+
+    img{
+        width: 60px;
+    }
+}
+    
 
 </style>
