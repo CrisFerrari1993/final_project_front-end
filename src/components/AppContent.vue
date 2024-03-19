c195510c9a3e1a5ac81c08da25d5df210e4cb913
Cristopher Ferrari <ferraricristopher93@gmail.com>
2024-03-19 20:26:49 +0100

    categories

--- src/components/AppContent.vue
+++ src/components/AppContent.vue
@@ -7,14 +7,15 @@
       <div class="row justify-content-center">
         <div
           v-for="category in categories"
+          :key="category.id"
           class="col-sm-3 col-md-3 col-xl-2 p-1"
         >
-          <label :for="category.name" />{{ category.name }}
+          <label :for="category.id">{{ category.name }}</label>
           <input
             type="checkbox"
             :id="category.id"
-            :name="category.name"
             :value="category.id"
+            v-model="selectedCategories"
           />
         </div>
       </div>
@@ -22,22 +23,12 @@
     <div class="container px-2">
       <div class="row">
         <div
-          v-for="restaurant in restaurants"
+          v-for="restaurant in filteredRestaurants"
           :key="restaurant.id"
           class="col-sm-12 col-md-4 col-xl-3 px-2 py-3"
-          @click="getLinkId()"
+          @click="getMyRest(restaurant)"
         >
-          <div
-            class="card_link"
-            @click="
-              getMyRest(
-                restaurant.id,
-                restaurant.name,
-                restaurant.adress,
-                restaurant.logo
-              )
-            "
-          >
+          <div class="card_link">
             <div class="container resturant_card">
               <img
                 class="card-img-top"
@@ -56,56 +47,62 @@
     </div>
   </section>
 </template>
+
 <script>
 import axios from 'axios';
+
 export default {
   name: 'AppContent',
   data() {
     return {
-      selectedCategories: [],
       restaurants: [],
       categories: [],
+      selectedCategories: [],
     };
   },
   methods: {
     getImageUrl(logo) {
       return `http://127.0.0.1:8000/storage/${logo}`;
     },
-    getLinkId(restaurants) {
-      console.log(this.restaurants.id);
-    },
-    getMyRest(id, name, address, logo) {
-      this.idRestaurant = id;
-      this.nameRestaurant = name;
-      this.addressRestaurant = address;
-      this.logoRestaurant = logo;
+    getMyRest(restaurant) {
+      this.idRestaurant = restaurant.id;
+      this.nameRestaurant = restaurant.name;
+      this.addressRestaurant = restaurant.address;
+      this.logoRestaurant = restaurant.logo;
       this.ref = !this.ref;
     },
-    fillCategoryArray(category) {
-      this.selectedCategories.push(category.id);
+    fetchCategoriesAndRestaurants() {
+      axios
+        .all([
+          axios.get('http://127.0.0.1:8000/api/categories'),
+          axios.get('http://127.0.0.1:8000/api/restaurants'),
+        ])
+        .then(
+          axios.spread((categoriesRes, restaurantsRes) => {
+            this.categories = categoriesRes.data.results;
+            this.restaurants = restaurantsRes.data.results;
+            console.log('Categories:', this.categories);
+            console.log('Restaurants:', this.restaurants);
+          })
+        )
+        .catch(err => {
+          console.error('Error fetching data:', err);
+        });
+    },
+  },
+  computed: {
+    filteredRestaurants() {
+      if (this.selectedCategories.length === 0) {
+        return this.restaurants;
+      } else {
+        return this.restaurants.filter(restaurant => {
+          return this.selectedCategories.includes(restaurant.category_id);
+        });
+      }
     },
   },
   mounted() {
-    axios
-      .get('http://127.0.0.1:8000/api/categories')
-      .then(res => {
-        const data = res.data.results;
-        this.categories = data;
-        console.log(this.categories);
-      })
-      .catch(err => {
-        console.log(err);
-      }),
-      axios
-        .get('http://127.0.0.1:8000/api/restaurants')
-        .then(res => {
-          const data = res.data.results;
-          this.restaurants = data;
-          console.log(this.restaurants);
-        })
-        .catch(err => {
-          console.log(err);
-        });
+    this.fetchCategoriesAndRestaurants();
   },
 };
 </script>
